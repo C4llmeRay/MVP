0dd7f58073eafb962077a48d3ff45ff2f14f2428
Ramy Ben Chaalia <chaaliaramy@gmail.com>
2024-04-16 13:23:01 +0200

    testing push

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,31 +1,29 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { body, validationResult } = require('express-validator');
-const User = require('../models/User');
-const AuthController = require('../controllers/authController')
+const { body, validationResult } = require("express-validator");
+const User = require("../models/User");
+const AuthController = require("../controllers/authController");
 
 // Register a new user
-router.get('',  AuthController.getCurrentUser)
+router.get("", AuthController.getCurrentUser);
 
 router.post(
-  '/register',
+  "/register",
 
-    AuthController.registerUser
+  AuthController.registerUser
 );
 
-// User login 
- router.post('/login', AuthController.loginUser
-);
+// User login
+router.post("/login", AuthController.loginUser);
 
 // Get user by ID
-router.get('/:id', AuthController.getUserById);
+router.get("/:id", AuthController.getUserById);
 
 // Update user by ID
-router.put('/:id', AuthController.updateUser);
+router.put("/:id", AuthController.updateUser);
 
+//this is for push testing
 
-
-
-module.exports= {
-  routes:router
-}
+module.exports = {
+  routes: router,
+};
